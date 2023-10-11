e1d6684eae1dc54771e661bc39c1fcd9142219c7
dzuokumor <d.zuokumor@alustudent.com>
2023-10-11 04:47:51 -0700

    hello

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p negpod_3-q1
 
-mv student-list_0923.txt negpod_3-q1/
+mv students-list_0923.txt negpod_3-q1/
 mv select-emails.txt negpod_3-q1/
 mv main.sh negpod_3-q1/
 mv student-emails.txt negpod_3-q1/
