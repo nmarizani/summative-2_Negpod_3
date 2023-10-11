8f20cddaf8359daa2731a76bc0fc89c384fd7deb
nmarizani <n.marizani@alustudent.com>
2023-10-11 05:02:57 -0700

    Updated

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -2,7 +2,7 @@
 mkdir -p negpod_3-q1
 
 mv students-list_0923.txt negpod_3-q1/
-mv select-emails.txt negpod_3-q1/
+mv select-emails.sh negpod_3-q1/
 mv main.sh negpod_3-q1/
 mv student-emails.txt negpod_3-q1/
 
