c28292fff3020a8aa650596d68db616cc702b6d5
dzuokumor <d.zuokumor@alustudent.com>
2023-10-11 04:33:31 -0700

    moving directory

---
+++ move-to-directory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p negpod_3-q1
+
+mv student-list_0923.txt negpod_3-q1/
+mv select-emails.txt negpod_3-q1/
+mv main.sh negpod_3-q1/
+mv student-emails.txt negpod_3-q1/
+
+echo "The moving is done"
